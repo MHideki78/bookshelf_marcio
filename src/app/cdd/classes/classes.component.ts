af3dcc402c4cd2e95c645d9e8ebc1458f8d7c7e2
Mhideki78 <mhkanazawa@gmail.com>
2022-01-11 14:06:56 -0300

    adição de servicos e and point

--- src/app/cdd/classes/classes.component.ts
+++ src/app/cdd/classes/classes.component.ts
@@ -9,18 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassesComponent implements OnInit {
 
-  livrosGeneros: Generos[]=[
-    {_idGenero:"0", nomeGenero:"Generalidades", decimalGenero:"00", livrosGenero:450},
-    {_idGenero:"1", nomeGenero:"Filosofia e Psicologia", decimalGenero:"00", livrosGenero:125},
-    {_idGenero:"2", nomeGenero:"Religiao", decimalGenero:"00", livrosGenero:366},
-    {_idGenero:"3", nomeGenero:"Ciencias Socias", decimalGenero:"00", livrosGenero:450},
-    {_idGenero:"4", nomeGenero:"Linguas", decimalGenero:"00", livrosGenero:123},
-    {_idGenero:"5", nomeGenero:"Ciencias naturais e Matematica", decimalGenero:"00", livrosGenero:523},
-    {_idGenero:"6", nomeGenero:"Tecnologias e Ciencias Aplicadas", decimalGenero:"00", livrosGenero:325},
-    {_idGenero:"7", nomeGenero:"Artes", decimalGenero:"00", livrosGenero:523},
-    {_idGenero:"8", nomeGenero:"Literatura e Retorica", decimalGenero:"00", livrosGenero:655},
-    {_idGenero:"9", nomeGenero:"Geografia, Historia e Biografia", decimalGenero:"00", livrosGenero:570},
-  ];
+  livrosGeneros: Generos[]=[];
   visaoColunas=['_idGenero', 'nomeGenero' , 'decimalGenero']
 
   constructor() { }
---
+++ src/app/cdd/service/generos.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GenerosService {
+
+  private readonly urlAPI='/assents/generos.json';
+
+  constructor() { }
+
+  listagemGeneros(){
+
+  }
+}
