b165b02e011a3ab316811b9d5e9616bb9160665b
Mhideki78 <mhkanazawa@gmail.com>
2022-01-17 13:22:17 -0300

    inserindo Nav.Ba e usuario

--- src/app/cdd/service/generos.service.ts
+++ src/app/cdd/service/generos.service.ts
@@ -1,22 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Generos } from './../modelos/generos';
 import { HttpClient } from '@angular/common/http';
-import { Generos } from '../modelos/generos';
-import { delay } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { delay, first, tap} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenerosService {
 
-    private readonly urlAPI='/assets/generos.json';
+  private readonly urlAPI = '/assets/generos.json';
 
   constructor(private clienteDados: HttpClient) { }
 
   listagemGeneros() {
-
     return this.clienteDados.get<Generos[]>(this.urlAPI)
     .pipe(
-      delay(3000)
+      first(),
+      delay(500),
+      tap(apiGeneros => console.log(apiGeneros))
     )
   }
 }
