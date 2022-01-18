6b4cca31cdbefce6872779e94a2724c35f53afd0
Mhideki78 <mhkanazawa@gmail.com>
2022-01-18 17:40:18 -0300

    navegação e dashboard

---
+++ src/app/servicosInterface/dashboard.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { first, tap } from 'rxjs';
+
+import { Dashboard } from './../modelosInterface/dashboard';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+  private readonly uriAPI = '../../assets/dashboard.json'
+
+  constructor(private cadsDashboard: HttpClient) { }
+
+  listagemmenu(){
+    return this.cadsDashboard.get<Dashboard[]>(this.uriAPI)
+    .pipe(
+      first(),
+      tap(apiDashboard => console.log(apiDashboard))
+    )
+  }
+}
